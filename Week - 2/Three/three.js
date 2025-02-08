a7f31bd3ad12449181b70dfaa06c78dd60dfd264
saif <ssaaiiffjjaammaall@gmail.com>
2025-02-08 18:27:58 +0300

    added week two and week three

---
+++ Week - 2/Three/three.js
@@ -0,0 +1,7 @@
+// `I'm In
+// \\
+// Love \\ """ '''
+// ++ With ++
+// \"""\"""
+// ""JavaScript""``
+console.log("\`I'm in \n \\\\ \n love \\\\ \"\"\" \'\'\' \n \+\+ with \+\+ \n \\\"\"\"\\\"\"\" \n \"\"JavaScript\"\"\`\`")
---
+++ Week - 3/4/four.js
@@ -0,0 +1,11 @@
+let points = 10;
+++points
+++points
+++points
+console.log(points); // 13
+--points
+--points
+--points
+--points
+--points
+console.log(points); // 8;
