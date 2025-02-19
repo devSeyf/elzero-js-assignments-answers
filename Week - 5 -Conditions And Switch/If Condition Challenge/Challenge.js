cad0e222cb8f319998825303c1fbe03ed7a4264d
saif <ssaaiiffjjaammaall@gmail.com>
2025-02-19 13:45:49 +0300

    Challenge

---
+++ Week - 5 -Conditions And Switch/If Condition Challenge/Challenge.js
@@ -0,0 +1,32 @@
+/*
+  If Condition Challenge
+*/
+// Ternary operator (Condition)? True :false
+
+// let a = 10;
+//   a < 10
+//     ? console.log(10)
+//     : a >= 10 && a <= 40
+//     ? console.log("10 To 40")
+//     : a > 40
+//     ? console.log("> 40")
+//     : console.log("Unknown")
+
+
+    // Write Previous Condition With Ternary If Syntax
+
+let st = "Elzero Web School";
+
+if (st.length === "34") {
+  console.log("Good");
+}
+
+// W Position May Change
+if (st.charAt(st.indexOf("W")).toLowerCase === "w") {
+  console.log("Good");
+}
+
+if ( typeof( st.length ) !== "string") {
+  console.log("Good");
+}
+
