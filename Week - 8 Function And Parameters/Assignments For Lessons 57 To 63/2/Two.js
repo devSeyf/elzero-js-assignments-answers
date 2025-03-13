ace63e344eac9a9f5df6e5b7672e7fc32c906d2b
saif <ssaaiiffjjaammaall@gmail.com>
2025-03-13 23:20:13 +0300

    Added Empty Files Week 8

---
+++ Week - 8 Function And Parameters/Assignments For Lessons 57 To 63/2/Two.js
@@ -0,0 +1,25 @@
+let start = 10;
+let end = 0;
+let stop = 3;
+
+// Output
+// 10
+// 09
+// 08
+// 07
+// 06
+// 05
+// 04
+// 03
+
+for (let i = start; i >= stop; i--) {
+    if (i<start) {
+        console.log(`${end}${i}`);
+        
+    }
+    else{
+        console.log(i);
+        
+    }
+    
+}
